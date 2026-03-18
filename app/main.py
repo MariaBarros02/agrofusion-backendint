cff9f7f0acfe144ac01448a09389a1108851ad25
María Alejandra Barros Murillo <141078532+MariaBarros02@users.noreply.github.com>
2026-03-18 14:53:40 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -18,7 +18,7 @@ app = FastAPI(
 )
 # Orígenes permitidos para solicitudes CORS (frontend)
 origins = [
-    "https://inmero.co/agrofusion"
+    "https://inmero.co/agrofusion/test"
     "http://localhost:5173",
     "http://localhost:3000",
     "http://127.0.0.1:5173",
